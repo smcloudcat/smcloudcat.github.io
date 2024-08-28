228f87b91b0f21ce17cbbc2d9f7772026609aad8
云猫 <77085867+smcloudcat@users.noreply.github.com>
2024-08-28 22:17:31 +0800

    Update config.js

--- javascripts/config.js
+++ javascripts/config.js
@@ -1,11 +1,11 @@
-var disqus_shortname = 'hooloodemo';			//Short Name of disqus, this parameter is for disqus comment box.
+var disqus_shortname = '云猫的小窝';			//Short Name of disqus, this parameter is for disqus comment box.
 var hostbase = 'https://yuncatblog.pages.dev';		//The absolute root address of your blog without '/' in the end.
-var githubname = 'hooloo';						//Your username of GitHub.
+var githubname = 'yuncat';						//Your username of GitHub.
 var repos = 'yuncatblog.pages.dev';					//Name of repository that contains your blog.
-var sitetitle = 'Hooloo';						//Name of your blog.
+var sitetitle = '云猫的小窝';						//Name of your blog.
 var rss = '';									//Rss address of your blog.
-var suffix = '';								//Suffix of post files, '.' should be included.
-var branch = '';								//Branch of your blog repository. 'master' is default if you left it blank.
+var suffix = '.md';								//Suffix of post files, '.' should be included.
+var branch = 'gh-pages';								//Branch of your blog repository. 'master' is default if you left it blank.
 
 //Sample Config:
 //var disqus_shortname = 'sneezry';
