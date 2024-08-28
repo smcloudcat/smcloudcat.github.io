5c02a4455ee7cd4a9dfc072e641cd382021835dd
云猫 <77085867+smcloudcat@users.noreply.github.com>
2024-08-29 00:16:49 +0800

    Update getmd.js

--- javascripts/getmd.js
+++ javascripts/getmd.js
@@ -36,7 +36,10 @@ function main(){
 		showpost(path);
 		(function() {
             var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+            dsq.src = '//proxy.hancat.work/hancat/https/' + disqus_shortname + '.disqus.com/embed.js';
+
+            
+            
             (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
         })();
 	}
@@ -180,7 +183,7 @@ function showlist(list){
 	(function () {
         var s = document.createElement('script'); s.async = true;
 		s.type = 'text/javascript';
-        s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+        s.src = '//proxy.hancat.work/hancat/https/' + disqus_shortname + '.disqus.com/count.js';
         (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
     }());
 }
