94b842e1633713ed46d22b0cda9ad3ae03eaf482
ximami <3522934828@qq.com>
2024-09-01 13:13:06 +0800

    优化

--- javascripts/getmd.js
+++ javascripts/getmd.js
@@ -1,10 +1,18 @@
 var path = decodeURIComponent(location.hash.substr(2));
 var page = 1;
-if(location.search.substr(1,19)=='_escaped_fragment_='){
+
+if (location.search.substr(1, 19) == '_escaped_fragment_=') {
     path = decodeURIComponent(location.search.substr(20).split('&')[0]);
 }
-if(path == '/'){path = ''; window.history.replacetate(null, '', '/');page=1;}
-else if(path && !location.search){window.history.replaceState(null, '', (isroot?'':('/'+repos))+'/#!'+path);}
+
+if (path == '/') {
+    path = '';
+    window.history.replaceState(null, '', '/');
+    page = 1;
+} else if (path && !location.search) {
+    window.history.replaceState(null, '', (isroot ? '' : ('/' + repos)) + '/#!/' + path);
+}
+
 var converter = new Showdown.converter();
 var content = document.getElementById('content');
 var loading = document.getElementById('loading');
@@ -13,200 +21,155 @@ var xmlhttp;
 var kw;
 var postList;
 var pending;
-var commentscount = new Array();
-var isroot=((repos.indexOf('github.com')==-1 && repos.indexOf('github.io')==-1)?false:true);
+var commentscount = [];
+var isroot = (repos.indexOf('github.com') == -1 && repos.indexOf('github.io') == -1 ? false : true);
 
 main();
 
-function main(){
+function main() {
     var disqusCounts = document.getElementsByName('commentscount');
-    for(var i=0; i<disqusCounts.length; i++){
+    for (var i = 0; i < disqusCounts.length; i++) {
         commentscount[Number(disqusCounts[i].id.substr(5))] = disqusCounts[i].innerText;
     }
     content.innerHTML = '';
     loading.style.display = 'block';
-    if(path.split('/')[1] == 'search'){
+    if (path.split('/')[1] == 'search') {
         search(path.split('/')[2]);
-    }
-    else if(path && path.split('/')[1] != 'page'){
+    } else if (path && path.split('/')[1] != 'page') {
         showpost(path);
-    }
-    else{
+    } else {
         document.title = sitetitle;
-        if(postList){
+        if (postList) {
             showlist(postList);
-        }
-        else{
+        } else {
             pending = true;
             document.getElementById('takinglonger').style.display = 'none';
             chktakinglonger();
             var el = document.createElement('script');
-            el.src = 'https://proxy.hancat.work/hancat/https/api.github.com/repos/' + githubname + '/' + repos + '/contents/md?callback=showlist'+(branch?('&ref='+branch):'');
+            el.src = 'https://proxy.hancat.work/hancat/https/api.github.com/repos/' + githubname + '/' + repos + '/contents/md?callback=showlist' + (branch ? ('&ref=' + branch) : '');
             document.getElementsByTagName('head')[0].appendChild(el);
         }
     }
 }
 
-function home(){
+function home() {
     path = '';
-    if(page==1){
-        window.history.pushState(null, '', (isroot?'':('/'+repos))+'/');
-    }
-    else{
-        path = '/page/'+page;
-        window.history.pushState(null, '', (isroot?'':('/'+repos))+'/#!/page/'+page);
+    if (page == 1) {
+        window.history.pushState(null, '', (isroot ? '' : ('/' + repos)) + '/');
+    } else {
+        path = '/page/' + page;
+        window.history.pushState(null, '', (isroot ? '' : ('/' + repos)) + '/#!/page/' + page);
     }
     main();
 }
 
-function lowerCase(path){
+function lowerCase(path) {
     path = path.split('%');
-    newPath = path[0];
-    for(var i=1; i<path.length; i++){
-        newPath += '%'+path[i].substr(0,2).toLowerCase()+path[i].substr(2);
+    var newPath = path[0];
+    for (var i = 1; i < path.length; i++) {
+        newPath += '%' + path[i].substr(0, 2).toLowerCase() + path[i].substr(2);
     }
     return newPath;
 }
 
-function loadXMLDoc(url){
-    var xmlhttp=null;
-    if (window.XMLHttpRequest){// code for IE7, Firefox, Opera, etc.
-        xmlhttp=new XMLHttpRequest();
-    }
-    else if (window.ActiveXObject){// code for IE6, IE5
-        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    if (xmlhttp!=null){
+function loadXMLDoc(url) {
+    xmlhttp = new XMLHttpRequest();
+    if (xmlhttp != null) {
         pending = true;
         document.getElementById('takinglonger').style.display = 'none';
         chktakinglonger();
-        xmlhttp.onreadystatechange = function (){
-            if (xmlhttp.readyState==4){// 4 = "loaded"
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4) { // 4 = "loaded"
                 pending = false;
                 document.getElementById('takinglonger').style.display = 'none';
                 loading.style.display = 'none';
-                if (xmlhttp.status==200){// 200 = "OK"
+                if (xmlhttp.status == 200) { // 200 = "OK"
                     var blog_text = xmlhttp.responseText;
                     var encoded = false;
-                    if(blog_text.substr(0,2)=='::'){
+                    if (blog_text.substr(0, 2) == '::') {
                         blog_text = Base64.decode(blog_text.substr(2));
                         encoded = true;
-                    };
+                    }
                     blog_text = filterJekyllHeader(blog_text);
-                    var converter = new Showdown.converter();
-                    content.innerHTML = '<div id="content_inner"><div id="back_home"><a href="/" onclick="home();return false;">'+sitetitle+'</a><span>&nbsp;›&nbsp;</span></div><br><blockquote class="layui-elem-quote"><h2 id="post_title">' + decodeUtf8(getPostName(path)) + (encoded?Base64.decode('PHN1cCBzdHlsZT0iZm9udC1zaXplOjAuNWVtO3ZlcnRpY2FsLWFsaWduOiBzdXBlcjsiIHRpdGxlPSLmraTmlofnq6Dlt7Looqvph43mlrDnvJbnoIHku6XourLpgb/lrqHmn6UiPuKYmuiiq+e8lueggeeahOWGheWuuTwvc3VwPg=='):'') + '</h2><div class="date">' + pdate + '</div></blockquote><div id="sidebar" class="layui-col-md15">  ' + converter.makeHtml(blog_text) + '</div></div>';
-                }
-                else if(xmlhttp.status==404) {
-                    document.title = 'Not Found - '+sitetitle;
+                    content.innerHTML = '<div id="content_inner"><div id="back_home"><a href="/" onclick="home();return false;">' + sitetitle + '</a><span>&nbsp;›&nbsp;</span></div><br><blockquote class="layui-elem-quote"><h2 id="post_title">' + decodeUtf8(getPostName(path)) + (encoded ? Base64.decode('PHN1cCBzdHlsZT0iZm9udC1zaXplOjAuNWVtO3ZlcnRpY2FsLWFsaWduOiBzdXBlcjsiIHRpdGxlPSLmraTmlofnq6Dlt7Looqvph43mlrDnvJbnoIHku6XourLpgb/lrqHmn6UiPuKYmuiiq+e8lueggeeahOWGheWuuTwvc3VwPg==') : '') + '</h2><div class="date">' + pdate + '</div></blockquote><div id="sidebar" class="layui-col-md15">  ' + converter.makeHtml(blog_text) + '</div></div>';
+                } else if (xmlhttp.status == 404) {
+                    document.title = 'Not Found - ' + sitetitle;
                     content.innerHTML = '<img src="images/despicable_me.png" />';
-                }
-                else {
-                    document.title = 'Technology Problem - '+sitetitle;
+                } else {
+                    document.title = 'Technology Problem - ' + sitetitle;
                     content.innerHTML = '<div id="takinglonger"><blockquote>We meet a problem when try to handle ' + path + ' (Err: ' + xmlhttp.status + ').</blockquote></div>';
                 }
             }
         }
-        xmlhttp.open("GET",url,true);
+        xmlhttp.open("GET", url, true);
         xmlhttp.send(null);
     }
 }
 
-function chktakinglonger(){
-    setTimeout(function(){
-        if(pending){
+function chktakinglonger() {
+    setTimeout(function () {
+        if (pending) {
             document.getElementById('takinglonger').style.display = 'block';
         }
     }, 10000);
 }
 
-function showpost(path){
-    var url = location.protocol + '//' + location.hostname + (isroot?'':('/'+repos))+'/md/' + path.substr(1).replace(/\//g, '-')+(suffix?suffix:'');
-    document.title = decodeUtf8(getPostName(path)) + ' - '+sitetitle;
-    pdate = path.substr(1).split('/')[0]+'-'+path.substr(1).split('/')[1]+'-'+path.substr(1).split('/')[2];
+function showpost(path) {
+    var url = location.protocol + '//' + location.hostname + (isroot ? '' : ('/' + repos)) + '/md/' + path.substr(1).replace(/\//g, '-') + (suffix ? suffix : '');
+    document.title = decodeUtf8(getPostName(path)) + ' - ' + sitetitle;
+    pdate = path.substr(1).split('/').join('-');
     loadXMLDoc(url);
 }
 
-function showlist(list){
-    if(path.split('/')[1] == 'page'){
+function showlist(list) {
+    if (path.split('/')[1] == 'page') {
         page = Number(path.split('/')[2]);
-        if(isNaN(page) || page < 1){
+        if (isNaN(page) || page < 1) {
             page = 1;
         }
-        if(page == 1){
-            window.history.replaceState(null, '', (isroot?'':('/'+repos))+'/');
+        if (page == 1) {
+            window.history.replaceState(null, '', (isroot ? '' : ('/' + repos)) + '/');
         }
     }
     pending = false;
     document.getElementById('takinglonger').style.display = 'none';
     postList = list;
     var txt = '';
-    if(page*20-20>=list.data.length && page!=1){
-        page = Math.ceil(list.data.length/20);
-        window.history.replaceState(null, '', (isroot?'':('/'+repos))+'/#!/page/'+page);
+    if (page * 20 - 20 >= list.data.length && page != 1) {
+        page = Math.ceil(list.data.length / 20);
+        window.history.replaceState(null, '', (isroot ? '' : ('/' + repos)) + '/#!/page/' + page);
     }
-    for(var i = list.data.length-(page-1)*20; i > 0 && i > list.data.length-page*20; i--){
-        if(suffix && list.data[i-1].name.substr(-suffix.length)==suffix){
-            list.data[i-1].name = list.data[i-1].name.substr(0, list.data[i-1].name.length-suffix.length);
+    for (var i = list.data.length - (page - 1) * 20; i > 0 && i > list.data.length - page * 20; i--) {
+        if (suffix && list.data[i - 1].name.substr(-suffix.length) == suffix) {
+            list.data[i - 1].name = list.data[i - 1].name.substr(0, list.data[i - 1].name.length - suffix.length);
         }
-        txt += '<blockquote class="layui-elem-quote"><postlist><a href="'+(isroot?'':('/'+repos))+'/#!/' + encodePath(list.data[i-1].name, true) + '">' + getPostName(list.data[i-1].name) + '</a><div class="post_info"><span class="post_date">'+list.data[i-1].name.split('-')[0]+'-'+list.data[i-1].name.split('-')[1]+'-'+list.data[i-1].name.split('-')[2]+'</span></div></postlist></blockquote>';
+        txt += '<blockquote class="layui-elem-quote"><postlist><a href="' + (isroot ? '' : ('/' + repos)) + '/#!/' + encodePath(list.data[i - 1].name, true) + '">' + getPostName(list.data[i - 1].name) + '</a><div class="post_info"><span class="post_date">' + list.data[i - 1].name.split('-')[0] + '-' + list.data[i - 1].name.split('-')[1] + '-' + list.data[i - 1].name.split('-')[2] + '</span></div></postlist></blockquote>';
     }
-    if(page==1 && page*20<list.data.length){
-        txt += '<postlist><a class="prev_page" href="'+(isroot?'':('/'+repos))+'/#!/page/'+(page+1)+'">←较早的文章</a><div style="clear:both"></div></postlist>';
+    if (page == 1 && page * 20 < list.data.length) {
+        txt += '<postlist><a class="prev_page" href="' + (isroot ? '' : ('/' + repos)) + '/#!/page/' + (page + 1) + '">←较早的文章</a><div style="clear:both"></div></postlist>';
+    } else if (page > 1 && page * 20 >= list.data.length) {
+        txt += '<postlist><a class="next_page" href="' + (isroot ? '' : ('/' + repos)) + '/#!/page/' + (page - 1) + '">较新的文章→</a><div style="clear:both"></div></postlist>';
+    } else if (page > 1 && page * 20 < list.data.length) {
+        txt += '<postlist><a class="prev_page" href="' + (isroot ? '' : ('/' + repos)) + '/#!/page/' + (page + 1) + '">←较早的文章</a><a class="next_page" href="' + (isroot ? '' : ('/' + repos)) + '/#!/page/' + (page - 1) + '">较新的文章→</a><div style="clear:both"></div></postlist>';
     }
-    else if(page>1 && page*20>=list.data.length){
-        txt += '<postlist><a class="next_page" href="'+(isroot?'':('/'+repos))+'/#!/page/'+(page-1)+'">较新的文章→</a><div style="clear:both"></div></postlist>';
-    }
-    else if(page>1 && page*20<list.data.length){
-        txt += '<postlist><a class="prev_page" href="'+(isroot?'':('/'+repos))+'/#!/page/'+(page+1)+'">←较早的文章</a><a class="next_page" href="'+(isroot?'':('/'+repos))+'/#!/page/'+(page-1)+'">较新的文章→</a><div style="clear:both"></div></postlist>';
-    }
+    content.innerHTML = '<div id="content_inner"><div id="back_home"><a href="/" onclick="home();return false;">' + sitetitle + '</a><span>&nbsp;›&nbsp;</span></div><div id="posts">' + txt + '</div></div>';
     loading.style.display = 'none';
-    content.innerHTML = txt;
 }
 
-function search(keywords){
-    kw = keywords;
-    var url = location.protocol + '//' + location.hostname + (isroot?'':('/'+repos))+'/search.json';
-    loadXMLDoc(url);
+function encodePath(path, isName) {
+    var ret = encodeURIComponent(path);
+    if (!isName) {
+        ret = ret.replace(/%2F/g, '/');
+    }
+    return ret;
 }
 
-function searchCallback(list){
-    var txt = '<div id="back_home"><a href="/" onclick="home();return false;">'+sitetitle+'</a><span>&nbsp;›&nbsp;</span><a>搜索：'+kw+'</a></div><br>';
-    var post = new Array();
-    for(var i = 0; i < list.data.length; i++){
-        if(decodeURIComponent(list.data[i].name).toLowerCase().indexOf(decodeURIComponent(kw).toLowerCase()) != -1){
-            post.push(i);
-        }
-    }
-    if(post.length){
-        for(var j = post.length - 1; j >= 0; j--){
-            txt += '<blockquote class="layui-elem-quote"><postlist><a href="'+(isroot?'':('/'+repos))+'/#!/' + encodePath(list.data[post[j]].name, true) + '">' + getPostName(list.data[post[j]].name) + '</a><div class="post_info"><span class="post_date">'+list.data[post[j]].name.split('-')[0]+'-'+list.data[post[j]].name.split('-')[1]+'-'+list.data[post[j]].name.split('-')[2]+'</span></div></postlist></blockquote>';
-        }
-    }
-    else{
-        txt += '<blockquote class="layui-elem-quote">没有找到与「'+kw+'」相关的内容。<blockquote>'
-    }
-    loading.style.display = 'none';
-    content.innerHTML = txt;
+function getPostName(path) {
+    return path.split('/').pop().replace(/-/g, ' ').replace(/\d{4}/, function (m) { return m + '年'; }).replace(/\d{2}/, function (m) { return m + '月'; }).replace(/\d{2}/, function (m) { return m + '日'; });
 }
 
-function filterJekyllHeader(blog_text){
-    var jekyllSeparator = blog_text.indexOf('---', 4);
-    if(jekyllSeparator != -1){
-        blog_text = blog_text.substr(jekyllSeparator+3);
-    }
-    return blog_text;
-}
-
-function encodePath(path, lowerCaseOnly){
-    path = encodeURIComponent(path);
-    if(lowerCaseOnly){
-        path = lowerCase(path);
-    }
-    return path.replace(/%2F/g, '/');
-}
-
-function getPostName(path){
-    return decodeURIComponent(path.split('/').pop().split('-').slice(3).join('-')).replace(/^\s+|\s+$/g, '');
+function filterJekyllHeader(text) {
+    return text.replace(/^---[\s\S]*?---\s*/, '');
 }
 
 function decodeUtf8(text) {
@@ -214,6 +177,30 @@ function decodeUtf8(text) {
         return decodeURIComponent(escape(text));
     } catch (e) {
         console.error("Error decoding URI component:", e);
-        return text; // Return the original text if decoding fails
+        return text;
+    }
+}
+
+function search(keyword) {
+    kw = keyword;
+    document.title = sitetitle + ' - 搜索结果';
+    var url = 'https://proxy.hancat.work/hancat/https/api.github.com/search/code?q=' + encodeURIComponent(keyword) + '+repo:' + githubname + '/' + repos + '&callback=searchCallback';
+    var script = document.createElement('script');
+    script.src = url;
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
+
+function searchCallback(result) {
+    if (result.data.items.length > 0) {
+        var txt = '<div id="content_inner"><div id="back_home"><a href="/" onclick="home();return false;">' + sitetitle + '</a><span>&nbsp;›&nbsp;</span></div><div id="search_results">';
+        for (var i = 0; i < result.data.items.length; i++) {
+            txt += '<blockquote class="layui-elem-quote"><a href="' + (isroot ? '' : ('/' + repos)) + '/#!/' + encodePath(result.data.items[i].path) + '">' + result.data.items[i].path + '</a><div class="search_result">' + result.data.items[i].name + '</div></blockquote>';
+        }
+        txt += '</div></div>';
+        content.innerHTML = txt;
+        loading.style.display = 'none';
+    } else {
+        content.innerHTML = '<div id="content_inner"><div id="back_home"><a href="/" onclick="home();return false;">' + sitetitle + '</a><span>&nbsp;›&nbsp;</span></div><div id="search_results"><p>没有找到匹配的结果。</p></div></div>';
+        loading.style.display = 'none';
     }
 }
